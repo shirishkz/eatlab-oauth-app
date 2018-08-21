90b33a7c764d58512d7f5a639de8eb57981d4ea7
Shirish Kayastha <kayastha.shirish@outlook.com>
2018-08-21 23:09:47 +0700

    Added Procfile, Remove email validation

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -16,7 +16,7 @@ class OmniauthCallbacksController < ApplicationController
 
     @user = @identity.user || current_user
     if @user.nil?
-      @user = User.create(email: @identity.email || "")
+      @user = User.create(email: @identity.email || '')
       @identity.update_attribute(:user_id, @user.id)
     end
 
@@ -30,7 +30,7 @@ class OmniauthCallbacksController < ApplicationController
       sign_in_and_redirect @user, event: :authentication
       flash[:success] = "Welcome, #{@identity.name}!"
     else
-      session["devise.#{provider}_data"] = env["omniauth.auth"]
+      session["devise.#{provider}_data"] = env['omniauth.auth']
       redirect_to new_user_registration_url
       flash[:warning] = 'There was an error while trying to authenticate you...'
     end
--- app/models/form_user.rb
+++ app/models/form_user.rb
@@ -2,7 +2,7 @@ class FormUser < User
   attr_accessor :current_password
 
   validates_presence_of   :email, if: :email_required?
-  validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+  # validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
   validates_format_of     :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
 
   validates_presence_of     :password, if: :password_required?
